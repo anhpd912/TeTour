740c2d819012a21ecb634032bc9abae7829dc040
ducanhwork <pdanh.work@gmail.com>
2026-01-12 22:45:13 +0700

    init: backend

---
+++ backend/src/test/java/danh/dev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package danh.dev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
